1c8025c861958b53682a9e6bb7fe83b7d0535890
bcbowen <bcbowen@gmail.com>
2024-10-27 15:57:26 -0400

    One error left, man

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -24,7 +24,7 @@ def block_to_block_type(block : str) -> str:
 
     if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return block_type_heading
-    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+    if lines[0].startswith("```") and lines[-1].endswith("```"):
         return block_type_code
     if block.startswith(">"):
         for line in lines:
@@ -108,7 +108,7 @@ def heading_to_html_node(block):
 def code_to_html_node(block):
     if not block.startswith("```") or not block.endswith("```"):
         raise ValueError("Invalid code block")
-    text = block[4:-3]
+    text = block[3:-3]
     children = text_to_children(text)
     code = ParentNode("code", children)
     return ParentNode("pre", [code])

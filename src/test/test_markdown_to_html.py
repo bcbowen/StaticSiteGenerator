0051e4acdc6dd49054f172a92a13bbcfc9d65a75
bcbowen <bcbowen@gmail.com>
2024-10-27 15:31:10 -0400

    Fixed most tests

--- src/test/test_markdown_to_html.py
+++ src/test/test_markdown_to_html.py
@@ -5,68 +5,69 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from textnode import TextNode
+from markdown_blocks import markdown_to_html_node
 
-class TestMarkdownToHtml(unittest.TestCase):
+class TestMarkdownToHtmlNode(unittest.TestCase):
     def test_markdown_to_html_simple_paragraph(self): 
         block = "This is a paragraph"
-        html = TextNode.markdown_to_html(block)
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "p")
-        self.assertEqual(html.children[0].value, block)
+        self.assertEqual(html.children[0].children[0].value, block)
 
     def test_markdown_to_html_simpleH1(self):
         value = "This is a H1"
         block = f"# {value}"
-        html = TextNode.markdown_to_html(block)
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "h1")
-        self.assertEqual(html.children[0].value, value)
+        self.assertEqual(html.children[0].children[0].value, value)
 
     def test_markdown_to_html_simpleH2(self):
         value = "This is a H2"
         block = f"## {value}"
-        html = TextNode.markdown_to_html(block)
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "h2")
-        self.assertEqual(html.children[0].value, value)   
+        self.assertEqual(html.children[0].children[0].value, value)   
 
     def test_markdown_to_html_simpleH3(self):
         value = "This is a H3"
         block = f"### {value}"
-        html = TextNode.markdown_to_html(block)
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "h3")
-        self.assertEqual(html.children[0].value, value)       
+        self.assertEqual(html.children[0].children[0].value, value)       
 
     def test_markdown_to_html_simpleH6(self):
         value = "This is a H6"
         block = f"###### {value}"
-        html = TextNode.markdown_to_html(block)
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "h6")
-        self.assertEqual(html.children[0].value, value)          
+        self.assertEqual(html.children[0].children[0].value, value)          
 
     def test_markdown_to_html_simple_code(self):
         value = "10 goto 10"
         block = f"```{value}```"
-        html = TextNode.markdown_to_html(block)
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "pre")
         pre = html.children[0]
         self.assertEqual(len(pre.children), 1)
         self.assertEqual(pre.children[0].tag, "code")
-        self.assertEqual(pre.children[0].value, value) 
+        self.assertEqual(pre.children[0].children[0].value, value) 
 
     def test_markdown_to_html_simple_quote(self):
-        block = "> First of all, you're fired!" \
-        "> Yeah, I kinda figured that, Glenn"
-        html = TextNode.markdown_to_html(block)
+        block = """> First of all, you're fired!
+> Yeah, I kinda figured that, Glenn"""
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "blockquote")
@@ -77,9 +78,9 @@ class TestMarkdownToHtml(unittest.TestCase):
         self.assertEqual(lines[1], "Yeah, I kinda figured that, Glenn")
     
     def test_markdown_to_html_simple_unordered_list(self):
-        block = "* dental floss" \
-        "* grappeling hook"
-        html = TextNode.markdown_to_html(block)
+        block = """* dental floss
+* grappeling hook"""
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "ul")
@@ -91,9 +92,9 @@ class TestMarkdownToHtml(unittest.TestCase):
         self.assertEqual(ul.children[1].value, "grappeling hook")
 
     def test_markdown_to_html_simple_ordered_list(self):
-        block = "1. steal socks" \
-        "2. profit"
-        html = TextNode.markdown_to_html(block)
+        block = """1. steal socks 
+2. profit"""
+        html = markdown_to_html_node(block)
         self.assertEqual(html.tag, "div")
         self.assertEqual(len(html.children), 1)
         self.assertEqual(html.children[0].tag, "ol")
@@ -105,35 +106,34 @@ class TestMarkdownToHtml(unittest.TestCase):
         self.assertEqual(ul.children[1].value, "profit")
 
     def test_markdown_to_html_big_shebang(self): 
-        markdown = "# Tolkien Fan Club" \
-        "" \
-        "**I like Tolkien**. Read my [first post here](/majesty) (sorry the link doesn\'t work yet)" \
-        "All that is gold does not glitter" \
-        "## Reasons I like Tolkien" \
-        "" \
-        "* You can spend years studying the legendarium and still not understand its depths" \
-        "* It can be enjoyed by children and adults alike" \
-        "* Disney *didn't ruin it*" \
-        "* It created an entirely new genre of fantasy" \
-        "## My favorite characters (in order)" \
-        "" \
-        "1. Gandalf" \
-        "2. Bilbo" \
-        "3. Sam" \
-        "4. Glorfindel" \
-        "5. Galadriel" \
-        "6. Elrond" \
-        "7. Thorin" \
-        "8. Sauron" \
-        "9. Aragorn" \
-        "Here's what `elflang` looks like (the perfect coding language):" \
-        "" \
-        "```" \
-        "func main(){" \
-        "    fmt.Println(\"Hello, World!\")" \
-        "" \
-        "```"
-        html = TextNode.markdown_to_html(markdown)
+        markdown = """# Tolkien Fan Club
+
+**I like Tolkien**. Read my [first post here](/majesty) (sorry the link doesn\'t work yet)
+All that is gold does not glitter
+## Reasons I like Tolkien"
+
+* You can spend years studying the legendarium and still not understand its depths
+* It can be enjoyed by children and adults alike
+* Disney *didn't ruin it*
+* It created an entirely new genre of fantasy
+## My favorite characters (in order)
+
+1. Gandalf
+2. Bilbo
+3. Sam
+4. Glorfindel
+5. Galadriel
+6. Elrond
+7. Thorin
+8. Sauron
+9. Aragorn
+Here's what `elflang` looks like (the perfect coding language):
+```
+    func main(){
+        fmt.Println(\"Hello, World!\")
+    }
+```"""
+        html = markdown_to_html_node(markdown)
         self.assertEqual(html.tag, "div")
         self.assertEqual(html.children[0].tag, "h1")
 
